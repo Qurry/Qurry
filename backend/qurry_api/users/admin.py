7fb62999b071ad76fdce7f5dbb5bcfcf78c96e6c
Abdualltif Ghajar <ghajara7@gmail.com>
2020-10-14 13:49:22 +0200

    create custom user

---
+++ backend/qurry_api/users/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import UserCreationForm, UserChangeForm
+from .models import User
+
+
+class UserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = User
+    list_display = ('name', 'points', 'is_staff', 'is_active',)
+    list_filter = ('email', 'name', 'points', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('name', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+         ('Other', {'fields': ('points',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('name', 'email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email', 'name')
+    ordering = ('email',)
+
+
+admin.site.register(User, UserAdmin)
