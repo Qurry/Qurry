b5b650ffd430841d270d4dfa16bff02c936b1491
Tom Richter <tomrichter99@gmail.com>
2020-10-16 14:52:14 +0200

    change db from postgres to sqlite3, add test question view

--- backend/qurry_api/qurry_api/settings.py
+++ backend/qurry_api/qurry_api/settings.py
@@ -75,18 +75,8 @@ DATABASES = {
 
     'default': {
 
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'postgres',
-
-        'USER': 'abdu',
-
-        'PASSWORD': '1234',
-
-        'HOST': '127.0.0.1',
-
-        'PORT': '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'qurry-db',
     }
 
 }
