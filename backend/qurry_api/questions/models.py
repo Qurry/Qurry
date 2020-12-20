fc7695c36c1953cf7e8b31d74fa0029ea7c7cee7
Abdualltif Ghajar <ghajara7@gmail.com>
2020-12-20 19:49:01 +0100

    replace id with uuid

--- backend/qurry_api/questions/models.py
+++ backend/qurry_api/questions/models.py
@@ -1,7 +1,11 @@
+import uuid
+
 from django.db import models
 
 
 class Tag(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     name = models.CharField('Name', max_length=20)
     description = models.TextField('Description', blank=True, null=True)
@@ -11,6 +15,8 @@ class Tag(models.Model):
 
 
 class Question(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     title = models.CharField('Title', max_length=200)
     body = models.TextField('Body', max_length=500)
@@ -23,7 +29,7 @@ class Question(models.Model):
     vote_down_users = models.ManyToManyField("users.User", verbose_name='Users who voted down this question', related_name='question_downvotes', blank=True)
 
     def __str__(self):
-        return self.title
+        return '%s: %s' % (self.id, self.title)
 
     def count_votes(self):
         return len(self.vote_up_users.all()) -len(self.vote_down_users.all())
@@ -32,15 +38,14 @@ class Question(models.Model):
         return len(self.answer_set.all())
 
     def get_tag_id_list(self):
-        return list(self.tags.values_list('id', flat=True))
+        return list(str(id) for id in self.tags.values_list('id', flat=True))
     
     def as_preview(self):
         return {
-            'id': str(self.id),
+            'id': self.id,
             'title': self.title,
-            'body': self.body,
             'votes': self.count_votes(),
-            'anwers': self.count_answers(),
+            'answers': self.count_answers(),
             'tagIds': self.get_tag_id_list(),
             'user': self.user.as_preview(),
             'dateTime': self.date_time
@@ -48,17 +53,19 @@ class Question(models.Model):
 
     def as_detailed(self):
         return {
-            'id': str(self.id),
+            'id': self.id,
             'title': self.title,
             'body': self.body,
             'votes': self.count_votes(),
-            'anwers': list(answer.as_preview() for answer in self.answer_set.all()),
+            'answers': list(answer.as_preview() for answer in self.answer_set.all()),
             'tagIds': self.get_tag_id_list(),
             'user': self.user.as_preview(),
             'dateTime': self.date_time
         }
 
 class Answer(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     body = models.TextField('Body', max_length=500)
 
@@ -78,7 +85,7 @@ class Answer(models.Model):
 
     def as_preview(self):
         return {
-            'id': str(self.id),
+            'id': self.id,
             'body': self.body,
             'votes': self.count_votes(),
             'user': self.user.as_preview()
