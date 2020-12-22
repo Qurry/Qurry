c0db673126c7701fc38dad76c0bdc5dfe35aa0f4
Abdullatif Ghajar <abdullatif.ghajar@student.hpi.uni-potsdam.de>
2020-12-22 13:13:51 +0100

    fix db file location

--- backend/qurry_api/qurry_api/settings.py
+++ backend/qurry_api/qurry_api/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
 
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'qurry-db',
+        'NAME': BASE_DIR.parent / 'qurry-db',
     }
 
 }
