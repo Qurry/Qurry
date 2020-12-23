66795f7f8aaef17dc46162862a44702cad1a73ca
Abdullatif Ghajar <abdullatif.ghajar@student.hpi.uni-potsdam.de>
2020-12-23 23:34:10 +0100

    add Profiel class

--- backend/qurry_api/users/models.py
+++ backend/qurry_api/users/models.py
@@ -1,20 +1,21 @@
 import uuid
 
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin,Permission, _user_get_permissions
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, Permission, _user_get_permissions
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 
 from .managers import UserManager
+from media.models import Image
 
 
 class User(AbstractBaseUser, PermissionsMixin):
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     email = models.EmailField('email address', unique=True)
-    username = models.CharField('username', max_length=50, null=True, unique=True)
-    score = models.IntegerField('score', default=0)
+    username = models.CharField(
+        'username', max_length=50, null=True, unique=True)
 
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
@@ -30,14 +31,25 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def as_preview(self):
         return {
-                'id': self.id,
-                'username': self.username
-            }
+            'id': self.id,
+            'username': self.username
+        }
 
     def is_owner_of(self, obj):
         return obj.user == self
 
+
 class Token(models.Model):
 
-    user = models.OneToOneField(User, verbose_name='user', on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, verbose_name='user', on_delete=models.CASCADE)
     token = models.TextField('token')
+
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(
+        User, verbose_name='User', on_delete=models.CASCADE)
+    score = models.IntegerField('Score', default=0)
+    image = models.ForeignKey(Image, verbose_name='Profile Image',
+                              blank=True, null=True, on_delete=models.SET_NULL)
