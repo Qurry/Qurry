aa4d981539a18ea1ed6913c51214377780fcfa04
Tom Richter <tomrichter99@gmail.com>
2020-12-24 11:04:43 +0100

    add Answer component, CreateEditAnswer interface

---
+++ frontend/components/Answer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>{{ answer.body }} by {{ answer.user.username }}</p>
+    <hr />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+import { CreateEditAnswer } from './../pages/questions/question.model'
+
+@Component
+export default class QuestionListCard extends Vue {
+  @Prop()
+  answer!: CreateEditAnswer[]
+}
+</script>
--- frontend/pages/questions/question.model.ts
+++ frontend/pages/questions/question.model.ts
@@ -1,27 +1,3 @@
-export interface PreviewQuestion {
-  id: string
-  title: string
-  votes: number
-  answers: number
-  dateTime: string
-  user: {
-    id: number
-    username: string
-  }
-  tagIds: string[]
-}
-
-export interface Answer {
-  id: string
-  body: string
-  dateTime: string
-  votes: number
-  user: {
-    id: number
-    username: string
-  }
-}
-
 export interface DetailQuestion {
   id: string
   title: string
@@ -37,8 +13,37 @@ export interface DetailQuestion {
   answers: Answer[]
 }
 
+export interface PreviewQuestion {
+  id: string
+  title: string
+  votes: number
+  answers: number
+  dateTime: string
+  user: {
+    id: number
+    username: string
+  }
+  tagIds: string[]
+}
+
 export interface CreateEditQuestion {
   title: string
   body: string
   tagIds: number[]
+}
+
+export interface Answer {
+  id: string
+  body: string
+  dateTime: string
+  votes: number
+  user: {
+    id: number
+    username: string
+  }
+  userVote: number
+}
+
+export interface CreateEditAnswer {
+  body: string
 }
