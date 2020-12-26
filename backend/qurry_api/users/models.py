d8f22f80deeca47104f94415ec665a8a90caf349
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2020-12-26 22:37:02 +0100

    change joinDate to registerDate

--- backend/qurry_api/users/models.py
+++ backend/qurry_api/users/models.py
@@ -19,7 +19,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    joined_at = models.DateTimeField(default=timezone.now)
+    registered_at = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
