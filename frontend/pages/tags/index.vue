8d412781d23fb4299ea976b16701ae1d47836491
Tom Richter <tomrichter99@gmail.com>
2020-12-26 09:32:28 +0100

    add tag table

--- frontend/pages/tags/index.vue
+++ frontend/pages/tags/index.vue
@@ -1,10 +1,14 @@
 <template>
   <v-row>
     <v-col>
-      <h1>Tags</h1>
-      <p v-for="tag of tags" :key="tag.id">
-        {{ tag.id }} - {{ tag.name }} - {{ tag.description }}
-      </p>
+      <h1 class="mb-3">Tags</h1>
+      <v-data-table
+        :headers="headers"
+        :items="tags"
+        :hide-default-footer="true"
+        mobile-breakpoint="0"
+        class="elevation-1"
+      ></v-data-table>
     </v-col>
   </v-row>
 </template>
@@ -16,5 +20,17 @@ import { Tag } from './tag.model'
 @Component
 export default class Dashboard extends Vue {
   tags: Tag[] = this.$store.state.tags
+
+  headers = [
+    {
+      text: 'Name',
+      value: 'name',
+    },
+    {
+      text: 'Description',
+      value: 'description',
+      sortable: false,
+    },
+  ]
 }
 </script>
