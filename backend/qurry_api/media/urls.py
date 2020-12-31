10a4ee7f3bb90a7906c0aad051c7b79667facd62
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2020-12-31 16:05:12 +0100

    add urls for media

---
+++ backend/qurry_api/media/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .views import FileView
+from .models import Image, Document
+
+urlpatterns = [
+    path('images/', FileView.as_view(Model=Image), name='view-images'),
+    path('images/<int:id>/', FileView.as_view(Model=Image), name='view-images'),
+    
+    path('documetns/', FileView.as_view(Model=Document), name='view-documents'),
+    path('documetns/<int:id>/', FileView.as_view(Model=Document), name='view-documents'),
+]
--- backend/qurry_api/questions/urls.py
+++ backend/qurry_api/questions/urls.py
@@ -1,5 +1,4 @@
-from django.urls import path, include
-from django.http import JsonResponse
+from django.urls import path
 
 from .views import QuestionView, TagView, AnswerView, CommentView
 
