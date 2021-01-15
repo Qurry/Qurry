99b6ae9266bf40c11d0a47c7bbe25841fc867191
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2021-01-15 22:36:08 +0100

    fix another bug

--- backend/qurry_api/qurry_api/settings.py
+++ backend/qurry_api/qurry_api/settings.py
@@ -5,7 +5,7 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 
-MODE = env('MOD', default='development')
+MODE = env('MODE', default='development')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
