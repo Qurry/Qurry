6420054b3ee98b662a6731655ca09314acefc0f1
Tom Richter <tomrichter99@gmail.com>
2021-01-30 08:53:25 +0100

    add tagTree to store and tag index page

--- frontend/pages/tags/index.vue
+++ frontend/pages/tags/index.vue
@@ -2,45 +2,26 @@
   <v-row>
     <v-col>
       <h1 class="mb-3">Tags</h1>
-      <div
-        v-for="tagCategory in tagCategories"
-        :key="tagCategory.id"
-        class="mt-3"
-      >
-        <h2 :style="{ backgroundColor: tagCategory.color }">
-          {{ tagCategory.name }}
-        </h2>
-        <p>{{ tagCategory.description }}</p>
-        <v-data-table
-          :headers="headers"
-          :items="tagCategory.tags"
-          :hide-default-footer="true"
-          mobile-breakpoint="0"
-          class="elevation-1"
-        ></v-data-table>
-      </div>
+      <v-treeview open-all :items="tagTree">
+        <template v-slot:label="{ item }">
+          <span>
+            <v-chip class="ma-2" :color="item.color ? item.color : '888'">
+              {{ item.name }}
+            </v-chip>
+            {{ item.description }}
+          </span>
+        </template>
+      </v-treeview>
     </v-col>
   </v-row>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
-import { TagCategory } from './tag.model'
+import { Tag } from './tag.model'
 
 @Component
 export default class Dashboard extends Vue {
-  tagCategories: TagCategory[] = this.$store.state.tagCategories
-
-  headers = [
-    {
-      text: 'Name',
-      value: 'name',
-    },
-    {
-      text: 'Description',
-      value: 'description',
-      sortable: false,
-    },
-  ]
+  tagTree: Tag[] = this.$store.state.tagTree
 }
 </script>
--- frontend/pages/tags/tag.model.ts
+++ frontend/pages/tags/tag.model.ts
@@ -1,16 +1,7 @@
 export interface Tag {
   id: string
   name: string
-  description?: string
-  color: string
-  tagCategoryId: string
-  tagCategoryName?: string
-}
-
-export interface TagCategory {
-  id: string
-  name: string
-  description?: string
-  color: string
-  tags: Tag[]
+  description: string
+  color?: string
+  children: Tag[]
 }
