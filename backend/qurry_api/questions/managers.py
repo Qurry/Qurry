43622d55e0d2cf3e00ae28b5b2a6e25a658d3b5b
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2021-02-06 22:46:02 +0100

    filter tags by ORing conditions

--- backend/qurry_api/questions/managers.py
+++ backend/qurry_api/questions/managers.py
@@ -7,24 +7,10 @@ class QuestionQuerySet(models.QuerySet):
         if not tags.exists():
             return self.all()
 
-        # between tag groups we AND and between tags we OR
-        tag_groups = []
-        while(tags.exists()):
-            first_tag = tags.first()
-            siblings = first_tag.get_siblings(include_self=True) & tags
-            siblings_id_list = list(siblings.values_list('id', flat=True))
-            # exclude choosen tags
-            tags = tags.exclude(id__in=siblings_id_list)
-            # add tags group
-            tag_groups.append(siblings_id_list)
-
-        result = self.all()
-        for tag_group in tag_groups:
-            result &= result.filter(tags__in=tag_group)
-        return result
+        return self.filter(tags__in=tags)
 
     def search(self, words):
-        if words == '':
+        if not words:
             return self.all()
 
         search_result = self.none()
