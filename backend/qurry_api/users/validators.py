d7c4ab0e4c2e11e8f282a2cb7f06484fcd607772
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2021-02-06 22:23:55 +0100

    accept only email addresses ending with hpi.de

--- backend/qurry_api/users/validators.py
+++ backend/qurry_api/users/validators.py
@@ -5,6 +5,6 @@ from django.utils.deconstruct import deconstructible
 
 @deconstructible
 class HPIEmailValidator(validators.RegexValidator):
-    regex = r'^[\w\.-]*@([\w\.-]+\.)?(hpi\.de|hpi\.uni-potsdam\.de)\Z'
-    message = 'You have to provide a valid HPI email address'
+    regex = r'^[\w\.-]*@([\w\.-]+\.)?hpi\.de\Z'
+    message = 'You have to provide a valid HPI email address like `chris.neinel@student.hpi.de`'
     flags = re.ASCII
