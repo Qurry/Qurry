1a40655f3bfc59a9483780484d4c4acd559e7959
Tom Richter <tomrichter99@gmail.com>
2021-04-12 14:04:25 +0200

    add basic notificationMenu, refactor MenuBar

---
+++ frontend/components/layout/MenuBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <v-btn to="/" text rounded>Qurry</v-btn>
+    <v-spacer></v-spacer>
+    <template v-if="$store.state.auth.loggedIn">
+      <v-btn to="/questions" text rounded>Questions</v-btn>
+      <v-btn to="/tags" text rounded>Tags</v-btn>
+      <span>
+        {{ userScore }} <v-icon color="#ffb300"> mdi-trophy </v-icon>
+      </span>
+      <NotificationMenu />
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-menu</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item>
+            <v-btn to="/profile" text rounded>Profile</v-btn>
+          </v-list-item>
+          <v-list-item>
+            <v-btn to="/users" text rounded>Users</v-btn>
+          </v-list-item>
+          <v-list-item>
+            <v-btn to="/logout" text rounded>Logout</v-btn>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </template>
+    <template v-else>
+      <v-btn to="/login" text rounded>Login</v-btn>
+      <v-btn to="/register" text rounded>Register</v-btn>
+    </template>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class MessageList extends Vue {
+  @Prop()
+  userScore = ''
+}
+</script>
